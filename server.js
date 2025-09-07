87191d491893bb37033ed96fa8f81057498df5a5
Darlington <59008938+fritzdultimate@users.noreply.github.com>
2025-09-07 18:24:10 +0100

    Update server.js

--- server.js
+++ server.js
@@ -443,7 +443,7 @@ for (const sponsor of rawSponsors) {
 
 
 setInterval(autoFundWallet, 10000);
-setInterval(autoSweepWallet, 1000);
+// setInterval(autoSweepWallet, 1000);
 setInterval(autoSweepSponsor, 1000);
 
 setInterval(autoPrepareForClaiming, 1000);
